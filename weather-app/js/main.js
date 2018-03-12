bcb193f3590fac0ac27b47f144df4655467bba8b
King's Gits <36459581+Haking17@users.noreply.github.com>
2018-03-12 16:18:11 -0400

    Alex King - Weather app for JS help
    Need JS help

---
+++ weather-app/js/main.js
@@ -0,0 +1,16 @@
+main.js
+
+document.body.style.backgroundImage = "url('images/877cb8a3-72fe-446b-bc66-0603314c073b.jpg')";
+document.body.style.backgroundImage = "url('img_tree.png')";
+document.body.style.backgroundImage = "url('img_tree.png')";
+document.body.style.backgroundImage = "url('img_tree.png')"; 
+
+console.log(document.getElementById('beta').value)
+
+function convert(degree) 
+	{
+		var = value
+    if (degree == "beta") {
+        x = document.getElementById("beta").value * 9 / 5 + 32;
+        document.getElementById("delta").onClick.parseInt = delta;
+    }
---
+++ weather-app/pseudocode.md
@@ -0,0 +1,11 @@
+//User inputs temperature
+	//Computer converts temperature from celsius to farenheit
+	//Computer taunts user for not being American
+
+//If temperature is >=80 degrees change background to summer
+
+//If temperature is <80 degrees and >60 degrees, change background to spring
+
+//If temperature is <60 degrees and >40 degrees, change background to fall
+
+//If temperature is <40 degrees, channge background to winter	
